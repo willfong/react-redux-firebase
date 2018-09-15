5b86768461a9e3b58a1551134bf770c1f57a7b06
Will Fong <w@r10e.xyz>
2018-09-15 12:47:48 +0800

    First round

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/reducers/Auth.js
@@ -0,0 +1,8 @@
+//import { FETCH_TODOS } from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
